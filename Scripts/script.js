f701ffaf9ed1dcb06b516350f95be4bad19354a0
airamtaleb <airamtaleba@gmail.com>
2022-10-12 22:57:41 +0200

    add florayfauna

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,12 +1,17 @@
 //cambiar background color
 $(document.body).on('click', function (e) {
     //si pulsamos en body pero no en hijos
-//    if (e.target == this) {
-      cambiarColor()
-//     }
+    if (e.target != e.previousSibling) {
+      //cambiarColor()
+     }
   })
 
- document.getElementsByTagName("body").add
+
+  var content = document.getElementsByClassName("content");
+  content.addEventListener("click", cambiarColor,false );
+
+  
+// document.getElementsByTagName("body").add
   
 //   var el = document.getElementsByTagName("body")[0];
 //   el.addEventListener("touchstart", cambiarColor, false);
